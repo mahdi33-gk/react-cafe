197dc337a35c2c3c6482a5b681897b8d70f5ca00
mehedi <mehedi25122003@gmail.com>
2025-03-03 19:13:45 +0600

    almost done card section

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const Blog = ({blog}) => {
-    const{title, coverImg, readingTime, author, authorPic, hashtags} = blog;
-    return (
-        <div>
-            <h2>Title:{title}</h2>
-            <img src={coverImg} alt="" />
+const Blog = ({ blog }) => {
+  const { title, coverImg, readingTime, author, authorPic, hashtags } = blog;
+  return (
+    <div>
+      
+      <img src={coverImg} alt="" />
+      
+      <div className="flex justify-between py-5 items-center">
+        <div className="flex gap-2 items-center">
+          <img className="w-14 rounded-full" src={authorPic} alt="" />
+          
+          <div className="flex flex-col gap-y-1">
+            <h1 className="text-2xl">{author}</h1>
+            <p className="text-[15px] text-gray-500">Mar 14 (4 Days ago)</p>
+          </div>
         </div>
-    );
+        <div>
+          <h1>{readingTime} min read</h1>
+        </div>
+      </div>
+      <h2 className="text-4xl font-bold pb-2">{title}</h2>
+      <p className="text-[16px] text-gray-500">{hashtags}</p>
+      
+    </div>
+  );
 };
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
-}
+  blog: PropTypes.object.isRequired,
+};
 
 export default Blog;
