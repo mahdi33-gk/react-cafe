6027ed109d67de7a5d7c0a3c286a898a0703e14f
mehedi <mehedi25122003@gmail.com>
2025-03-03 17:33:41 +0600

    blog title added

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
+    const{title} = blog;
     return (
         <div>
-            
+            <h2>{title}</h2>
         </div>
     );
 };
--- src/Blogs.jsx
+++ src/Blogs.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-
+import Blog from './Blog';
 const Blogs = () => {
-    const [bolgs, setBlogs] = useState([])
+    const [blogs, setBlogs] = useState([])
     useEffect(()=>{
         fetch('blogs.json')
         .then(res=> res.json())
@@ -10,8 +10,10 @@ const Blogs = () => {
 
     return (
         <div className='md:w-2/3 mx-5'>
-            <p> blogs Length: {bolgs.length}</p>
-            
+            <p> blogs Length: {blogs.length}</p>
+            {
+                blogs.map(blog => <Blog blog={blog} key={blog.id}></Blog>)
+            }
         </div>
     );
 };
