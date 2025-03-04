36ce231b4e846429453902f6fa3c8f0aac39c612
mehedi <mehedi25122003@gmail.com>
2025-03-04 15:13:02 +0600

    bookmarked section done

--- src/Document.jsx
+++ src/Document.jsx
@@ -4,7 +4,7 @@ const Document = ({bookmark}) => {
     const {title} = bookmark;
     return (
         <div>
-            <p>{title}</p>
+            <p className='p-4 rounded-lg mt-2 text-white text-xl bg-slate-500 font-semibold'>{title}</p>
         </div>
     );
 };
